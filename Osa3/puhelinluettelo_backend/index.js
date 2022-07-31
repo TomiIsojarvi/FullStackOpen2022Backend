dde3f0aec7b518ef2fb1aa168dc03896b096c7a5
Mona Lisa <isojarvi.tomi@gmail.com>
2022-07-31 15:36:37 +0300

    push

--- Osa3/puhelinluettelo_backend/index.js
+++ Osa3/puhelinluettelo_backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 // generateId - Generates an id for a person.
 const generateId = () => {
@@ -19,6 +20,7 @@ const generateId = () => {
 morgan.token('person', function (req, res) { return JSON.stringify(req.body )})
 
 // Middlewares
+app.use(cors())
 app.use(express.json())
 //app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
@@ -128,7 +130,7 @@ const unknownEndpoint = (req, res) => {
 app.use(unknownEndpoint)
 
 // Startup the server.
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
